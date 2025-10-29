c3f93e2497d257cde66903ae076a16145c31b265
root <root@ip-10-42-11-76.ec2.internal>
2025-10-29 14:52:45 +0000

    Fix Betty style for bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -4,31 +4,34 @@
  * bubble_sort - Sorts an array of integers in ascending order using Bubble sort
  * @array: The array to sort
  * @size: Number of elements in @array
+ *
+ * Description: Prints the array after each swap
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int temp;
-    int swapped;
+	size_t i, j;
+	int temp;
+	int swapped;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        swapped = 0;
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                print_array(array, size);
-                swapped = 1;
-            }
-        }
-        if (!swapped)
-            break;
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = 0;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+				swapped = 1;
+			}
+		}
+		if (swapped == 0)
+			break;
+	}
 }
+
