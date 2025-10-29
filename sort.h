56201d7cfcbbc759d7eabc75472be70ee1b1d6a3
sondos04 <sondos.alrubaih04@gmail.com>
2025-10-29 21:56:04 +0000

    add listint defenition

--- sort.h
+++ sort.h
@@ -4,6 +4,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * struct listint_s - Doubly linked list node
+ * @n: integer stored in the node
+ * @prev: points to the previous element of the list
+ * @next: points to the next element of the list
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
