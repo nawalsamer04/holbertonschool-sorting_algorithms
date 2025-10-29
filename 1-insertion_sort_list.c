76fb1a63069758f221671b6e0b0f7508d978d63b
root <root@ip-10-42-11-76.ec2.internal>
2025-10-29 16:49:05 +0000

    Implement insertion sort for doubly linked list

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ *                       in ascending order using Insertion sort
+ * @list: Double pointer to the head of the doubly linked list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *temp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	current = (*list)->next;
+	while (current)
+	{
+		temp = current;
+		while (temp->prev && temp->n < temp->prev->n)
+		{
+			/* Swap nodes */
+			if (temp->next)
+				temp->next->prev = temp->prev;
+			temp->prev->next = temp->next;
+			temp->next = temp->prev;
+			temp->prev = temp->next->prev;
+
+			if (temp->prev)
+				temp->prev->next = temp;
+			else
+				*list = temp;
+
+			temp->next->prev = temp;
+
+			print_list(*list);
+		}
+		current = current->next;
+	}
+}
